2041e45a7f6309ab963a3878db5748bb80fa9bcd
angela <angela@londonappbrewery.com>
2019-10-24 09:56:10 +0100

    Fix email textfield IBOutlet

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
-    @IBOutlet weak var emailTextfield: UIView!
+    @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
