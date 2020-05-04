3a00e5aee447fdfa30b961b176d12611924dd94a
Merouane Bellaha <m.bellaha@gmail.com>
2020-05-04 22:14:16 +0200

    add animateLogo()

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,14 +9,25 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
-    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet private weak var titleLabel: UILabel!
+    
+    private let logoName = "⚡️FlashChat"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        animateLogo()
     }
-    
 
+    private func animateLogo() {
+        logoName.enumerated().forEach { (logoNameEnumerated) in
+             let (index, character) = logoNameEnumerated
+             Timer.scheduledTimer(withTimeInterval: 0.1 * Double(index), repeats: false) { _ in
+                 self.titleLabel.text?.append(character)
+             }
+         }
+    }
 }
+
+
+
