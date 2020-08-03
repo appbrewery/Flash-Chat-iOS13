0779b0cbf0079028e1f0f3cc13659e8e53233dc3
Daniel Luo <d.luo117@gmail.com>
2020-08-02 17:43:18 -0700

    Animate title text with CLTypingLabel pod

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,28 +7,14 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        animateText()
+        titleLabel.text = "⚡️FlashChat"
     }
-
-    func animateText() {
-        let titleText = "⚡️FlashChat"
-        titleLabel.text = ""
-        var charIndex = 0.0
-
-        for char in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(char)
-            }
-            charIndex += 1
-        }
-    }
-    
-
 }
