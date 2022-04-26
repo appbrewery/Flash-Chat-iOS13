e2c06ab489a11f18597778b67fef6bcc778492eb
d.sankovsky <jasonantario@gmail.com>
2022-04-26 13:28:13 +0300

    Replacing welcome animation by animation from CLTypingLabel

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,21 +7,14 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
     
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let titleText = "⚡️FlashChat"
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        titleLabel.text = "⚡️FlashChat"
     }
 }
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '13.0'
+
+target 'Flash Chat iOS13' do
+    use_frameworks!
+    
+    pod 'CLTypingLabel', '~> 0.4.0'
+end
